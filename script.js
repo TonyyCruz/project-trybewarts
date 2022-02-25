3b7a47747673f7258996f6f26f278187b4b7c7b4
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-02-25 16:45:07 -0300

    Requisito 3 ok

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Variaveis Globais
 const btnEntrar = document.querySelector('.btn-entrar');
+
+// Funcoes
+
+function verificaEmailSenha(event) {
+  event.preventDefault();
+  const email = document.querySelector('#email');
+  const senha = document.querySelector('#senha');
+
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+// Eventos
+
+btnEntrar.addEventListener('click', verificaEmailSenha);
