899cd779fd27f6010c16230c177dad87ae95af70
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-02-25 18:38:29 -0300

    requisito 20 ok

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Variaveis Globais
 const btnEntrar = document.querySelector('.btn-entrar');
-// const textArea = document.querySelector('#textarea');
+const textArea = document.querySelector('#textarea');
+const divCounter = document.querySelector('#counter');
+const btnEnviar = document.querySelector('#agreement');
 
 // Funcoes
 
@@ -16,14 +18,35 @@ function verificaEmailSenha(event) {
   }
 }
 
-// function contador(event) {
-//   const divCounter = document.querySelector("#counter");
-//   let counter = 500;
-//   const numCarateres = event.target.value.length;
-//   counter -= numCarateres;
+function contador(event) {
+  let counter = 500;
+  const numCarateres = event.target.value.length;
+  counter -= numCarateres;
+  divCounter.innerText = counter;
+}
+
+// function criaBaseDados() {
+
+//   let dados ={};
+//   const nomeSobrenome = document.querySelector('#input-name').value + document.querySelector('#input-lastname').value;
+//   const email = document.querySelector('#input-email').value;
+//   const casa = document.querySelector('#house').value;
+//   const familia = 
+
+// }
+
+
+// function fixaInformacao(event) {
+//   event.preventDefault();
+//   const formulario = document.querySelector('#evaluation-form');
+//   const inputs = document.querySelectorAll("input")
+//   const info = document.createElement('p');
+//   const infoGeral = document.createElement('div');
+
 // }
 
 // Eventos
 
 btnEntrar.addEventListener('click', verificaEmailSenha);
-// textArea.addEventListener('keyup', contador);
+textArea.addEventListener('keyup', contador);
+// btnEnviar.addEventListener('click', fixaInformacao);
